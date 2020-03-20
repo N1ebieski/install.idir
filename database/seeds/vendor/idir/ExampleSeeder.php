b3eaa9537b0894e6a7479af9ea60cde2878b6899
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-03-20 13:00:15 +0100

    example seeds + factories for classmaps

---
+++ database/seeds/vendor/idir/ExampleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace N1ebieski\IDir\Seeds;
+
+use Illuminate\Database\Seeder;
+
+class ExampleSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
