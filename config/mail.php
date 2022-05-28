e82ee4495ba7b1872527556cf30b20ea8be817ba
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-05-28 14:52:29 +0200

    laravel 8 update

--- config/mail.php
+++ config/mail.php
@@ -113,6 +113,19 @@ return [
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
-    ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | If you are using the "log" driver, you may specify the logging channel
+    | if you prefer to keep mail messages separate from other log entries
+    | for simpler reading. Otherwise, the default channel will be used.
+    |
+    */
+
+    'log_channel' => env('MAIL_LOG_CHANNEL'),
 
 ];
